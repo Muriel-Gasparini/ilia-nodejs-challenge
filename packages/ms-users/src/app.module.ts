064d5355fd678a45545682e549845ccd19ad4ab7
Muriel Gasparini <muriel.developer@gmail.com>
2026-02-08 15:24:47 -0300

    feat(ms-users): integrate wallet endpoints

--- packages/ms-users/src/app.module.ts
+++ packages/ms-users/src/app.module.ts
@@ -5,9 +5,16 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TransactionsClientModule } from './transactions-client/transactions-client.module';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, TransactionsClientModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    TransactionsClientModule,
+    WalletModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

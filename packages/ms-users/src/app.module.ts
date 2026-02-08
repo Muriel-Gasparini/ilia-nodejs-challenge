ef24efc1fed38213249787ec7947cd7155cc9a93
Muriel Gasparini <muriel.developer@gmail.com>
2026-02-08 15:17:42 -0300

    feat(ms-users): add TransactionsClientModule

--- packages/ms-users/src/app.module.ts
+++ packages/ms-users/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TransactionsClientModule } from './transactions-client/transactions-client.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule],
+  imports: [PrismaModule, AuthModule, UsersModule, TransactionsClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
